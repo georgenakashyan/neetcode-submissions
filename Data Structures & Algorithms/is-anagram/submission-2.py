7d677873b0dcfcb793d2365c14d796e1fba71d82
George <91430969+georgenakashyan@users.noreply.github.com>
2026-06-01 23:20:46 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        letters = {}
+        for i in range(len(s)):
+            letters.update({s[i]: (letters.get(s[i]) or 0)+1})
+            if (letters.get(s[i]) == 0):
+                letters.pop(s[i])
+            letters.update({t[i]: (letters.get(t[i]) or 0)-1})
+            if (letters.get(t[i]) == 0):
+                letters.pop(t[i])
+        print(letters)
+        return len(letters) == 0
